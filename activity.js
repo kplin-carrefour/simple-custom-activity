da14324512a5685fd76a0fc2e8942934bd0d0dd5
kplin-carrefour <137925375+kplin-carrefour@users.noreply.github.com>
2023-06-27 15:58:50 -0300

    Add files via upload

--- activity.js
+++ activity.js
@@ -1,7 +1,9 @@
 var connection = new Postmonger.Session();
 
+connection.trigger('ready');
+
 connection.on('initActivity', function(data) {
-    document.getElementById('configuration').value = JSON.stringify(data); //, null, 2);
+    document.getElementById('configuration').value = JSON.stringify(data, null, 2);
 });
 
 connection.on('clickedNext', function(data) {
