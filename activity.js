92fd424365acf6b01b005c25b004920d68eb6b88
kplin-carrefour <137925375+kplin-carrefour@users.noreply.github.com>
2023-06-27 14:46:16 -0300

    Inicio

---
+++ activity.js
@@ -0,0 +1,10 @@
+var connection = new Postmonger.Session();
+
+connection.on('initActivity', function(data) {
+    document.getElementById('configuration').value = JSON.stringify(data, null, 2);
+});
+
+connection.on('clickedNext', function(data) {
+    var configuration = JSON.parse(document.getElementById('configuration').value);
+    connection.trigger('updateActivity', configuration);
+});
